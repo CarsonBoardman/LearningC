a5e6ea9f3d7f9360c9ec7a4047c18efba812ed7f
Carson Boardman <carsonibologna@gmail.com>
2022-11-30 18:26:32 +0100

    Fixed login, just needed more exp with strings

--- login/login.c
+++ login/login.c
@@ -5,40 +5,67 @@
 
 void registerUser();
 void loginUser();
-bool userInDatabase(char **);
+bool userInDatabase(char *);
+void start();
 char * readPass(FILE*);
 FILE *users;
 FILE *currentUser;
 int main(){
+    // NOTE: There must be a file called 'userfiles' with 
+    // the first byte a zero to start
     
-    
-    
-    {
-        char *response;
-        
-        printf("%s", "Login or Register? l/r\n");
-        scanf("%s", response);
-        if (*response == 114){
-        registerUser();
+    // Typing this I realise it's my job to make
+    // sure you don't have to deal with this sort of thing...
+    // I'll do it later
+    users = fopen("userfiles", "r+");
+    bool quit = false;
+    if (!users){
+        printf("%s", "Failed to open file\n");
+        return -1;
+    }
+    // While there's no user file open users 
+    // can log in or register
+    while (!currentUser){
+    start();
+    }
+    // Everything from here to the cleanup part is with the user logged into the file.
+    // until the user quits
+    while (!quit){
+        int r;
+        // Here is where functionallity goes
+        printf("%s", "1. quit\n");
+        scanf("%d", &r);
+        switch (r){
+            case 1:
+                quit = true;
+                break;
+            default:
+                break;
         }
     }
-// Now for the login functionality. Users will enter a username that will
-// be checked to see if its valid
-    loginUser();
-    // Everything from here to the cleanup part is with the user logged into the file.
-
     
     // Clean up open files
-    if (users == NULL){
-        
+    if (users == NULL){   
         fclose(users);
-        
     }
     if (currentUser == NULL){
         fclose(currentUser);
     }
-    
     return 0;
+}
+// Starting screen, choice to log in or
+// register     unsafe
+void start(){
+    printf("\033[2J\033[1;1H");
+    char response;        
+    printf("%s", "Login or Register? l/r\n");
+    scanf("%s", &response);
+    // If user typed 'r'
+    if (response == 114){
+    registerUser();
+    }
+    printf("\033[2J\033[1;1H");
+    loginUser();
 }
 // This function is made to register a new user and save their password 
 // to file. Obviously this isn't secure but I can do that later for another
@@ -48,100 +75,69 @@ int main(){
 // I'll figure out later
 void registerUser(){
     // Creates the strings that will be the new username and password
-    char * user[32];
-    char * pass[32];
+    char user[32];
+    char pass[32];
+    int len;
     printf("%s", "Username:\n");
-    scanf("%s", *user);
-    // I want to check if the username already exists
-    // I'll add that to TODO
-    /*
-    if (!userInDatabase(*user)){
-        printf("%s", "User already exists");
+    scanf("%s", user);
+    // checks if user already exists, see below
+    if (userInDatabase(user)){
+        printf("%s", "User already exists\n");
         return;
     }
-    */
     printf("%s", "Password:\n");
-    scanf("%s", *pass);
+    scanf("%s", pass);
     // Creates a new file with the name of the user
-    FILE *newFile = fopen(*user, "w");
+    FILE *newFile = fopen(user, "w");
     if (!newFile){
         printf("%s", "Failed to open file\n");
     }
-    users = fopen("userfiles", "w");
-    if (!users){
-        printf("%s", "Failed to open file\n");
-    }
-    printf("%s", "got to here\n");
-    {
-        // Homemade code to print password to 
-        // file, I know fprintf() exists but I'd 
-        // rather figure out a solution myself
-        char * passChar;
-        for (int i = 0; i < 31; i++){ // Will write 32 bytes in total
-            *pass[i] = *passChar;
-            if (*passChar == 0){
-            printf("%c", *pass[i]);
-            putc(pass[i], newFile);
-            } else {
-                putc(0, newFile); // fill the rest of 32 bytes with null
-            }
-        }
+    // Prints password to user file
+    // as well as changes the count 
+    // of users in 'userfiles'
+        fprintf(newFile, "%32s", pass);
+        rewind(users);
+        fscanf(users, "%d", &len);
+        len++;
+        rewind(users);
+        fprintf(users, "%d", len);
+        fseek(users, 0, SEEK_END);
+        fprintf(users, "%32s", user);
 
-    }
-    {
-        // Now record the username to the users file
-        // Similar deal but I don't need to fill up all 32 bytes
-        char * userChar;
-        for (int i = 0; i < 31; i++){ // Will write 32 bytes in total
-            *userChar = *user[i];
-            
-            putc(*user[i], users); // This inlcudes the null string terminator
-            if (*userChar == 0){
-                break;
-            }
-        }
-    }
     // Closes the files to clean up to login to new file
     fclose(newFile);
-    fclose(users);
     return;
     
 }
+// login function
 void loginUser(){
-    fpos_t * position;
     printf("%s", "Login:\n");
-    char * user[32];
-    char * pass[32];
-    char * passwordCheck[32];
+    // Sets up strings
+    char user[32];
+    char pass[32];
+    char passwordCheck[32];
     printf("%s", "Enter Username:\n");
-    
-    scanf("%s", *user);
-    printf("%s", *user);
-    
-    /*
-    if (!userInDatabase(*user)){
+    scanf("%s", user);
+    // checks if name exist so not to make
+    // a new file
+    if (!userInDatabase(user)){
         printf("%s", "Name not found\n");
         return;
     }
-    */
+
     printf("%s", "Enter Password: \n");
+    scanf("%s", pass);
+    // Opens user's file..
+    currentUser = fopen(user, "r");
+    // ...reads password
+    fscanf(currentUser, "%s", passwordCheck);
     
-    scanf("%s", *pass);
-    
-    printf("%s", "No problems yet\n");
-    currentUser = fopen(*user, "r");
-    printf("%s", "Password is:\n");
-    printf("%s", *pass);
-    *passwordCheck = readPass(currentUser);
-    
-    if (strcmp(*pass, *passwordCheck) == 0){
-        printf("%s", "youre in");
-        fgetpos(currentUser, position);
-        printf("%s", "file position is ");
-        printf("%ld", ftell(currentUser));
-        printf("%s", "\n");
+    if (strcmp(pass, passwordCheck) == 0){
+        fseek(currentUser, 32, SEEK_SET);
         return ;
     }else {
+        // If the password is wrong it will
+        // close the user's file
         printf("%s", "incorrect password \n");
         fclose(currentUser);
         return;
@@ -149,47 +145,24 @@ void loginUser(){
 
 }
 
-bool userInDatabase(char ** nameToCheck){
-    users = fopen("userfiles", "r");
-    if (!users){
-        printf("%s", "Failed to open file\n");
-    }
-    // TODO: check if username is in database
-    char * check;
-    rewind(users);
-
-    *check = fgetc(users);
-    
-
-    return true;
-    if (users != NULL){
-        fclose(users);
-    }
-}
-
-//function to read the password to a file
-char * readPass(FILE* file){
-    char * pass[32];
+// Function for testing if user exists
+// Returns {true} if it does
+bool userInDatabase(char* nameToCheck){
+    // All strings in the app are the same length
+    // this is important
+    char check[32];
     int len;
-    //make sure at the beginning of file
-    rewind(file);
-    /*
-    {
-        // The password is followed by nulls to fill up space
-        // I only need the password string
-        for (int i = 0; i < 32; i ++){
-            *pass[i] = fgetc(file);
-            if (*pass[i] == 0){ // I put the check here because I need one null as a string terminator
-                len = i;
-                break;
-            }
+    // Go to beginning of file
+    rewind(users);
+    // reads how many users there are
+    fscanf(users, "%d", &len);
+    // for each one reads if its the one
+    // we're looking for
+    for (int i = 0; i < len; i++){
+        fscanf(users, "%31s", check);
+        if (strcmp(check, nameToCheck) == 0){
+            return true;
         }
-    char * returnPass[len]; // If it breaks this is likily why, I think I should use malloc
-    
     }
-    */
-    fscanf(file, "%32s", *pass);
-    return *pass;
+    return false;  
 }
-
-
